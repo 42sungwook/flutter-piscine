7084b986ae73cfd16b522c5cbedf41e1690b1a60
Sungwook Kim <silenc3@naver.com>
2024-12-15 17:42:57 +0900

    feat: switch text message

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_piscine/config/theme.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  static const String _welcomeText = 'Welcome to Mobile Piscine';
+  static const String _helloText = 'Hello World!';
+
+  bool _isWelcomeText = true;
+
+  void _toggleText() {
+    setState(() {
+      _isWelcomeText = !_isWelcomeText;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,16 +31,14 @@ class HomeScreen extends StatelessWidget {
               child: Padding(
                 padding: EdgeInsets.all(AppTheme.spacing2x),
                 child: Text(
-                  'Welcome to Mobile Piscine',
+                  _isWelcomeText ? _welcomeText : _helloText,
                   style: Theme.of(context).textTheme.displayLarge,
                 ),
               ),
             ),
             SizedBox(height: AppTheme.spacing3x),
             ElevatedButton(
-              onPressed: () {
-                print('Button pressed');
-              },
+              onPressed: _toggleText,
               child: const Text('Button'),
             ),
           ],
